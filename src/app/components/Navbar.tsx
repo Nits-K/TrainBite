2ee1a338631d53c6fa2d71fed790895b35f02ad6
Nitin Kant <kantnitin7@gmail.com>
2024-06-27 20:20:32 +0530

    added hover effect

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,32 +1,58 @@
 import { FaHome, FaUsers, FaClipboardList, FaComments, FaUtensils, FaTrain } from "react-icons/fa";
-
+import React, { useEffect } from 'react';
+import "@/app/styles/navbar.css"
 export default function Navbar() {
+
+    document.addEventListener("click", (e:any)=>{
+      let el;
+      
+      if(e.target.nodeName === "path") {        
+        el = e.target.parentElement.parentElement;
+      }
+      else if(e.target.nodeName === "svg" || e.target.nodeName ==="SPAN") {
+
+        el=e.target.parentElement;
+        
+      }
+      else if(e.target.nodeName ==="P") {
+        el=e.target;
+      }
+      
+      
+      if (el && el.classList.length) {
+        if (el.classList.contains("nav-link") && !el.classList.contains("active")) {
+          document.getElementsByClassName("active")[0].classList.remove("active");
+          el.classList.add("active");
+        }
+    }
+  });
+  
   return (
-    <nav className="fixed sm:border-r-black sm:border-r sm:space-y-8 sm:flex-col sm:w-20 sm:h-full sm:py-4 sm:top-0 bottom-0 left-0 w-full h-20 bg-red-600 flex items-center p-2 justify-evenly sm:shadow-lg z-10 ">
-      <a href="/" className="text-white flex flex-col items-center transform transition-transform duration-100 hover:scale-125  ">
+    <nav className={`${window.innerWidth <= 640 ? "rect " : ""}fixed sm:border-r-black sm:border-r sm:space-y-8 sm:flex-col sm:w-16 sm:h-full sm:py-4 sm:top-0   bg-red-600 sm:shadow-lg z-10 `}>
+      <p  className="nav-link active text-white flex flex-col items-center transform transition-transform duration-100 hover:scale-125  ">
         <FaHome size={24} />
         <span className="text-xs mt-1">Home</span>
-      </a>
-      <a href="/" className="text-white flex flex-col items-center transform transition-transform duration-300 hover:scale-125">
+      </p>
+      <p className="nav-link text-white flex flex-col items-center transform transition-transform duration-300 hover:scale-125">
         <FaUsers size={24} />
         <span className="text-xs mt-1">Group</span>
-      </a>
-      <a href="/" className="text-white flex flex-col items-center transform transition-transform duration-300 hover:scale-125">
+      </p>
+      <p className="nav-link text-white flex flex-col items-center transform transition-transform duration-300 hover:scale-125">
         <FaClipboardList size={24} />
         <span className="text-xs mt-1">Orders</span>
-      </a>
-      <a href="/" className="text-white flex flex-col items-center transform transition-transform duration-300 hover:scale-125">
+      </p>
+      <p className="nav-link text-white flex flex-col items-center transform transition-transform duration-300 hover:scale-125">
         <FaTrain size={24} />
-        <span className="text-xs mt-1">Train Tools</span>
-      </a>
-      <a href="/" className="text-white flex flex-col items-center transform transition-transform duration-300 hover:scale-125">
+        <span className="text-xs text-center mt-1"> Tools</span>
+      </p>
+      <p className="nav-link text-white flex flex-col items-center transform transition-transform duration-300 hover:scale-125">
         <FaComments size={24} />
         <span className="text-xs mt-1">Chat</span>
-      </a>
-      <a href="/" className="text-white flex flex-col items-center transform transition-transform duration-300 hover:scale-125">
+      </p>
+      <p className="nav-link text-white flex flex-col items-center transform transition-transform duration-300 hover:scale-125">
         <FaUtensils size={24} />
         <span className="text-xs mt-1">Menu</span>
-      </a>
+      </p>
     </nav>
   );
 }
